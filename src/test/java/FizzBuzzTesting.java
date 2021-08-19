ea1c8fd98d076726cb9780e3fca86ba514ceb1b7
shrivmudit <muditsrivastava.2421@gmial.com>
2021-08-19 21:14:31 +0530

    Requirement 2: Print Fizz if it has 3 init or divisible by 3 and Print Buzz if it has 5 inti or divisible by 5

--- src/test/java/FizzBuzzTesting.java
+++ src/test/java/FizzBuzzTesting.java
@@ -31,5 +31,47 @@ public class FizzBuzzTesting {
         String actualoutcome = Obj.fizzorbuzz();
         assertEquals(expectedoutcome, actualoutcome);
     }
+    @DisplayName("When the Value is 29")
+    @Test
+    public void CheckWhenValueIsNotDivisibleByThreeAndFive() {
+        MethodsFizzBuzz Obj = new MethodsFizzBuzz(29);
+        String expectedoutcome = "29";
+        String actualoutcome = Obj.fizzorbuzz();
+        assertEquals(expectedoutcome, actualoutcome);
+    }
+
+    @DisplayName("When the Value is 83")
+    @Test
+    public void CheckWhenValueHasThreeInItButNotDivisibleBy3() {
+        MethodsFizzBuzz Obj = new MethodsFizzBuzz(83);
+        String expectedoutcome = "Fizz";
+        String actualoutcome = Obj.fizzorbuzz();
+        assertEquals(expectedoutcome, actualoutcome);
+    }
+
+    @DisplayName("When the Value is 85")
+    @Test
+    public void CheckWhenValueHasFiveInItButNotDivisibleBy5() {
+        MethodsFizzBuzz Obj = new MethodsFizzBuzz(85);
+        String expectedoutcome = "Buzz";
+        String actualoutcome = Obj.fizzorbuzz();
+        assertEquals(expectedoutcome, actualoutcome);
+    }
+    @DisplayName("When the Value is 70")
+    @Test
+    public void CheckWhenValueIsDivisibleBy5() {
+        MethodsFizzBuzz Obj = new MethodsFizzBuzz(70);
+        String expectedoutcome = "Buzz";
+        String actualoutcome = Obj.fizzorbuzz();
+        assertEquals(expectedoutcome, actualoutcome);
+    }
+    @DisplayName("When the Value is 70")
+    @Test
+    public void CheckWhenValueIsDivisibleBy() {
+        MethodsFizzBuzz Obj = new MethodsFizzBuzz(101);
+        String expectedoutcome = "Unexpected Input Value";
+        String actualoutcome = Obj.fizzorbuzz();
+        assertEquals(expectedoutcome, actualoutcome);
+    }
 
 }
